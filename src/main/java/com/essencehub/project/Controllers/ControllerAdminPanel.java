7483606ec9389e9bc0a8120121072c87c05db743
Yusuf Mert Deniz <denizyusuf238@gmail.com>
2024-11-16 15:11:12 +0300

    updated controllers

--- src/main/java/com/essencehub/project/Controllers/ControllerAdminPanel.java
+++ src/main/java/com/essencehub/project/Controllers/ControllerAdminPanel.java
@@ -1,4 +1,81 @@
 package com.essencehub.project.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
 public class ControllerAdminPanel {
+
+    @FXML
+    private HBox assignTaskPanel;
+
+    @FXML
+    private HBox dashboardPanel;
+
+    @FXML
+    private HBox employeePanel;
+
+    @FXML
+    private HBox financePanel;
+
+    @FXML
+    private ImageView msgIcon;
+
+    @FXML
+    private Label namePanel;
+
+    @FXML
+    private ImageView profilPicturePanel;
+
+    @FXML
+    private ImageView settingIcon;
+
+    @FXML
+    private Label statusPanel;
+
+    @FXML
+    private HBox stockTrackingPanel;
+
+    @FXML
+    void assignTaskPanelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dashboradClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void employeePanelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void financeClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void msgIconClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void profilePicturePanelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void settingIconClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void stockTrackingCLicked(MouseEvent event) {
+
+    }
+
 }
---
+++ src/main/java/com/essencehub/project/Controllers/ControllerEmployeePanel.java
@@ -0,0 +1,58 @@
+package com.essencehub.project.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+public class ControllerEmployeePanel {
+
+    @FXML
+    private HBox dashboardPanel;
+
+    @FXML
+    private ImageView msgIcon;
+
+    @FXML
+    private HBox performancePanel;
+
+    @FXML
+    private ImageView profilePicturePanel;
+
+    @FXML
+    private ImageView settingIcon;
+
+    @FXML
+    private HBox tasksPanel;
+
+    @FXML
+    void dashboardPanelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void msgIconCLicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void performancePanelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void profilePicturePanelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void settingsIconClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void tasksPanelClicked(MouseEvent event) {
+
+    }
+
+}
